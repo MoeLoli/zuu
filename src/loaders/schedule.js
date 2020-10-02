73cdecb4fe3b86de8c7ab4510aa945e733762b17
Jin <moeprprpr@gmail.com>
2020-10-02 18:04:37 +0800

    🚧 WIP(schedule): edit schedule export function

--- src/loaders/schedule.js
+++ src/loaders/schedule.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: Jin
+ * @Date: 2020-10-02 18:00:33
+ * @LastEditors: Jin
+ * @LastEditTime: 2020-10-02 18:03:17
+ * @FilePath: /zuu/src/loaders/schedule.js
+ */
 import schedule from 'node-schedule';
 
 export const schedules = [];
 
-export default schedule;
+export default () => {
+
+};

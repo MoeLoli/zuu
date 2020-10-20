a9f276cace57aa2c719a0798a3d81f27a72362cf
Jin <moeprprpr@gmail.com>
2020-10-20 13:06:12 +0800

    📦 refactor(koa2.js): modify the hook call method

--- src/loaders/koa2.js
+++ src/loaders/koa2.js
@@ -2,7 +2,7 @@
  * @Author: Jin
  * @Date: 2020-09-24 09:16:43
  * @LastEditors: Jin
- * @LastEditTime: 2020-10-03 21:29:54
+ * @LastEditTime: 2020-10-10 09:21:44
  * @FilePath: /zuu/src/loaders/koa2.js
  */
 import fs from 'fs';
@@ -43,42 +43,21 @@ const getRoutes = async (filePath) => {
     return fileList;
 }
 
-const routes = zuu.routes = zuu.routes ? zuu.routes : [];
-
-const trObj2Router = ({ name = '', url = '', method = 'get', callback = ((ctx, next) => { }), middleware = null }, app) => {
-    if (!name || !url || !method || !callback) return;
-
-    const RequestMethod = [
-        'get',
-        'post',
-        'pust',
-        'delete',
-        'option',
-        'patch'
-    ];
-    if (RequestMethod.indexOf(method) == -1) return;
-    const router = new KoaRouter();
-    if (middleware) {
-        router[method](url, middleware, callback);
-    } else {
-        router[method](url, callback);
-    }
-    const item = {
-        name: ((url.split('/').filter(e => e != ''))[0]).toLocaleLowerCase(),
-        url: url,
-        method: method,
-        middleware: middleware,
-        callback: callback
-    };
-    routes.push(item);
-    app.use(router.routes()).use(router.allowedMethods());
-}
+const routeList = zuu.routes = zuu.routes ? zuu.routes : [];
 
 export default async ({ app }) => {
+    const loadMiddleware = (...args) => {
+        app.use.apply(app, args);
+    }
+
+    trigger('REGISTER_MIDDLEWARE_1', [ loadMiddleware ]);
+
     app.use(middlewares.onError);
     app.use(middlewares.header);
     app.use(middlewares.accessControl);
     app.use(middlewares.statistics);
+
+    trigger('REGISTER_MIDDLEWARE_2', [ loadMiddleware ]);
 
     app.use(async (ctx, next) => {
         const start = new Date();
@@ -91,9 +70,32 @@ export default async ({ app }) => {
     app.use(koaBody());
     app.use(json());
 
+    trigger('REGISTER_MIDDLEWARE_3', [ loadMiddleware ]);
+
     // router
+    let routeNum = 0;
+
+    const regRouter = (method, ...args) => {
+        if (!method || args.length === 0) return
+        
+        routeNum++;
+        const router = new KoaRouter();
+        router[method].apply(router, args);
+        app.use(router.routes()).use(router.allowedMethods());
+        
+        routeList.push({
+            name: ((args[0].split('/').filter(e => e != ''))[0]).toLocaleLowerCase(),
+            args
+        })
+    }
+    
+    trigger('REGISTER_ROUTE_1', [ regRouter ]);
+
+    trigger('REGISTER_MIDDLEWARE_4', [ loadMiddleware ]);
+
     let routes = await getRoutes(path.join(__dirname, '../api/routes'));
     routes = routes.map(value => {
+        routeNum++;
         const name = value.substr(0, value.indexOf('.')).substr(value.lastIndexOf('/') + 1, value.length);
         const groupPathArr = (value.substr(value.indexOf('api/routes') + 'api/routes'.length + 1, value.length)).split('/');
         groupPathArr.pop();
@@ -106,23 +108,22 @@ export default async ({ app }) => {
         };
     });
     routes = routes.filter(value => fs.existsSync(value.path));
-
-    let routeNum = routes.length;
-
+    
     routes.forEach((value, index, array) => {
         array[index].object = require(value.path);
         app.use((array[index].object).routes(), (array[index].object).allowedMethods());
         logger.success(`Router [${value.group === '' ? value.name : value.group + ' - ' + value.name}] has been loaded`, 'ROUTER');
     }, this);
 
-    trigger('REGISTER_ROUTE_1', [ ((obj) => {
-        routeNum++;
-        trObj2Router(obj, app)
-    }) ]);
+    trigger('REGISTER_ROUTE_2', [ regRouter ]);
+
+    trigger('REGISTER_MIDDLEWARE_5', [ loadMiddleware ]);
 
     logger.info(util.format(`%d ${routeNum.length == 1 ? 'route' : 'routes'} have been loaded`, routeNum), 'SYSTEM');
 
     app.use(async () => {
         throw new HttpError(constants.HTTP_CODE.NOT_FOUND);
     });
+
+    trigger('REGISTER_MIDDLEWARE_6', [ loadMiddleware ]);
 };

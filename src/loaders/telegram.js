f381a0a26dc64e69e4841bea81199767d78d170d
Jin <moeprprpr@gmail.com>
2020-10-21 14:55:42 +0800

    ✨ feat(telegram.js): add proxy support
    Add proxy support to make it more suitable for the environment in Mainland China

--- src/loaders/telegram.js
+++ src/loaders/telegram.js
@@ -2,29 +2,38 @@
  * @Author: Jin
  * @Date: 2020-09-26 22:36:56
  * @LastEditors: Jin
- * @LastEditTime: 2020-10-03 21:39:46
+ * @LastEditTime: 2020-10-21 14:46:11
  * @FilePath: /zuu/src/loaders/telegram.js
  */
-import { Telegraf } from 'telegraf';
+import { Telegraf } from "telegraf";
+import HttpsProxyAgent from "https-proxy-agent";
 
-import config from '@/config';
-import { trigger } from './hook';
-
-/*emitter.addListener("pluginLoaded", () => {
-    bot.launch();
-});*/
+import config from "@/config";
+import { trigger } from "./hook";
+import logger from "./logger";
 
 export default () => {
-    if (!config?.telegram?.token) return ;
-    const bot = new Telegraf(config.telegram.token);
+    if (!config?.telegram?.token) return;
 
-    const genBotConfig = (obj) => {
-        if (obj?.method || obj?.content || obj?.middleware) return;
-
-        bot[obj.method](obj.content, obj.middleware);
+    const botConfig = {};
+    if (config.http_proxy) {
+        botConfig.telegram = {
+            agent: new HttpsProxyAgent(config.http_proxy),
+        };
     }
+    const bot = new Telegraf(config.telegram.token, botConfig);
+    bot.catch((err, ctx) => {
+        logger.error(
+            `Ooops, encountered an error for ${ctx.updateType} ${err}`,
+            "TELEGRAF"
+        );
+    });
+    const regTelegramBot = (method, ...args) => {
+        if (!method || args.length < 2) return;
 
-    trigger('REGISTER_ROUTE_1', [ genBotConfig ]);
-    
+        bot[method].apply(bot, args);
+    };
+    trigger("REGISTER_TELEGRAM_BOT_1", [regTelegramBot]);
+
     bot.launch();
-}
+};

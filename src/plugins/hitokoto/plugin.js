a66ba561ab2e15e54a238a722a2ec30d936e5edc
Jin <moeprprpr@gmail.com>
2020-10-21 14:40:09 +0800

    🚨 test(plugin.js): add demo plugin

--- src/plugins/hitokoto/plugin.js
+++ src/plugins/hitokoto/plugin.js
@@ -2,50 +2,51 @@
  * @Author: Jin
  * @Date: 2020-09-24 14:20:23
  * @LastEditors: Jin
- * @LastEditTime: 2020-10-03 09:40:54
+ * @LastEditTime: 2020-10-20 12:59:04
  * @FilePath: /zuu/src/plugins/hitokoto/plugin.js
  */
-import _ from 'lodash';
+import _ from "lodash";
 
-const hitokotoData = require('./hitokoto.json');
+const hitokotoData = require("./hitokoto.json");
 
 class Hitokoto {
     init() {
-        this.hook.register('REGISTER_ROUTE_1', [this, this.regRouter]);
+        this.hook.register("REGISTER_ROUTE_2", [this, this.regRoute]);
+        this.hook.register("REGISTER_TELEGRAM_BOT_1", [
+            this,
+            this.regTelegramBot,
+        ]);
     }
 
-    regDocs () {
-        return {
-            type: 'md',
-            content: require('fs').readFileSync(__dirname + '/docs.md', 'utf-8')
-        }
+    regTelegramBot(fn) {
+        fn("command", "/hitokoto", (ctx) => {
+            let hitokoto = _.chain(hitokotoData.hitokoto);
+            hitokoto = hitokoto.sampleSize(1).value();
+            ctx.reply(hitokoto[0].hitokoto, {
+                reply_to_message_id: ctx.message.message_id,
+            });
+        });
     }
 
-    regTelegramBot () {
-        return {
-            method: 'command',
-            content: '/hitokoto',
-            middleware: (ctx) => {
-                let hitokoto = _.chain(hitokotoData.hitokoto);
-                hitokoto = hitokoto.sampleSize(1).value();
-                ctx.reply(hitokoto[0].hitokoto, { reply_to_message_id: ctx.message.message_id });
-            }
-        }
-    }
+    regRoute(fn) {
+        fn("get", "/hitokoto/cat", (ctx) => {
+            ctx.status = 200;
+            ctx.body = {
+                status: "success",
+                data: hitokotoData.categories,
+            };
+        });
 
-    regRouter(fn) {
-        fn({
-            name: 'jjj ',
-            url: '/cat',
-            method: 'get',
-            callback: (ctx, next) => {
-                ctx.status = 200;
-                ctx.body = {
-                    status: 'success',
-                    data: hitokotoData.categories
-                };
-            }
-        })
+        fn("get", "/hitokoto", (ctx) => {
+            let hitokoto = _.chain(hitokotoData.hitokoto);
+            hitokoto = hitokoto.sampleSize(1).value();
+
+            ctx.status = 200;
+            ctx.body = {
+                status: "success",
+                data: hitokoto,
+            };
+        });
     }
 }
 
